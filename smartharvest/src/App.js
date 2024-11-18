43e088b2a4d5c93b5f0c1fff24001137f1e1b4e3
Maaz <maazkashif47@gmail.com>
2024-11-18 02:39:41 -0500

    first commit

---
+++ smartharvest/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import HarvestRewards from './components/HarvestRewards';
+
+function App() {
+  return (
+    <div className="App">
+      <HarvestRewards />
+    </div>
+  );
+}
+
+export default App;
